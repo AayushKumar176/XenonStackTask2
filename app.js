29e2f3508bfdcb327e8af626c19f845466c9b600
AayushKumar176 <aayushdbg2002@gmail.com>
2023-09-28 15:48:38 +0530

    cors error

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const DefaultData=require("./defaultdata");
 const cors= require("cors");
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors());
 
-// Use cors middleware with options
-const corsOptions = {
-    origin: 'https://6498441b7290f10e11e795a2--scintillating-muffin-9630b5.netlify.app', // Replace with your frontend domain
-    credentials: true, // Allow credentials (cookies, authorization headers)
-  };
+// // Use cors middleware with options
+// const corsOptions = {
+//     origin: 'https://6498441b7290f10e11e795a2--scintillating-muffin-9630b5.netlify.app', // Replace with your frontend domain
+//     credentials: true, // Allow credentials (cookies, authorization headers)
+//   };
   
-  app.use(cors(corsOptions)); // Apply CORS middleware with options
+//   app.use(cors(corsOptions)); // Apply CORS middleware with options
 
 const router=require("./routes/router");
 app.use(router);
